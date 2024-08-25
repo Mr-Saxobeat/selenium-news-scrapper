1f0b276f70133515d76949fc45d227681c065c72
Weiglas <weiglasribeiro@gmail.com>
2024-08-25 00:08:13 -0300

    chore: clear imports

--- bots/reuters.py
+++ bots/reuters.py
@@ -1,13 +1,10 @@
+import os
 import re
 from datetime import datetime
-from typing import Tuple
-from RPA.Browser.Selenium import Selenium
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver import Chrome
-
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import StaleElementReferenceException
 from selenium.webdriver.support import expected_conditions
