88f5585873f7c04498d2b460d049e0bcba1fec57
Weiglas <weiglasribeiro@gmail.com>
2024-08-25 10:04:52 -0300

    fix: fix chrome arguments

--- bots/reuters.py
+++ bots/reuters.py
@@ -38,11 +38,12 @@ class Reuters:
     def open_browser(self):
         options = Options()
         options.page_load_strategy = 'eager'
-        options.add_argument("--start-maximized")
-        options.add_argument("--disable-infobars")
+        # options.add_argument("--start-maximized")
+        # options.add_argument("--disable-infobars")
+        # options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--disable-extensions")
+        options.add_argument("--headless")
         options.add_argument("--disable-gpu")
-        options.add_argument("--disable-dev-shm-usage")
         options.add_argument("--no-sandbox")
         self.browser = Chrome(options=options)
 
